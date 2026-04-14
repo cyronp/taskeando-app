1991cda261948994ccc2705ddf1f0f9a3287f430
Cyronp <vitorhenriquedasilveira@gmail.com>
2026-04-13 22:49:52 -0300

    start: initialize next repository

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className="flex">
+        {/* Icon */}
+        <div className="">
+          <h1></h1>
+        </div>
+      </div>
+    </>
+  );
+}
